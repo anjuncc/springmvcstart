8da4184c7e50d31aa856c3a5e2fbe474a0b612e2
anjuncc <anjuncc@163.com>
2016-03-20 14:58:28 +0800

    to springmvcangularjs

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # springmvcstart
 Form Validation and Resource Handling
 @Size, @Past & @NotNull are standard annotations while @NotEmpty & @Email are not part of specification.
+@ModelAttribute
+
+ResponseEntity<domain>
+ResponseEntity<Void>
+Writing REST Client using RestTemplate
+Spring MVC @RequestBody, @ResponseBody annotations usage, ResponseEntity, default HttpMessageConverteres available, and writing custom message converters
--- src/main/java/a/j/stringmvcstart/config/AppConfig.java
+++ src/main/java/a/j/stringmvcstart/config/AppConfig.java
@@ -1,21 +1,39 @@
 package a.j.stringmvcstart.config;
 
+import java.util.List;
+
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 @Configuration
 @ComponentScan(basePackages="a.j.springmvcstart")
 @EnableWebMvc
 public class AppConfig extends WebMvcConfigurerAdapter{
-
+	   @Override
+	    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+	        converters.add(mappingJackson2HttpMessageConverter());
+	    }
+	     
+	    @Bean
+	    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+	        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+	        converter.setObjectMapper(new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false));
+	        return converter;
+	    }
+	     
     /*
      * Configure View Resolver
      */
